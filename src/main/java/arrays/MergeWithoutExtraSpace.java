6ec9fab90bdd49898f9973785ca541fd9c4f4fe6
mstoyanovca <mstoyanovca@gmail.com>
2021-02-20 19:50:01 -0500

    merge without extra space

--- src/main/java/arrays/MergeWithoutExtraSpace.java
+++ src/main/java/arrays/MergeWithoutExtraSpace.java
@@ -1,2 +1,60 @@
-package arrays;public class MergeWithoutExtraSpace {
+package arrays;
+
+import java.util.Arrays;
+
+public class MergeWithoutExtraSpace {
+    public void merge(int[] arr1, int[] arr2, int n, int m) {
+        if (n < 1 || n > 50_000) throw new IllegalArgumentException();
+        if (m < 1 || m > 50_000) throw new IllegalArgumentException();
+        for (int x : arr1) if (x < 0 || x > 10_000_000) throw new IllegalArgumentException();
+        for (int x : arr2) if (x < 0 || x > 10_000_000) throw new IllegalArgumentException();
+
+        int gap = gap(n + m);
+
+        while (gap >= 1) {
+            for (int i = 0; i + gap < n + m; i++) {
+                if (i + gap < n) {
+                    // comparison within arr1:
+                    if (arr1[i] > arr1[i + gap]) swap(arr1, i, i + gap);
+                } else if (i > n - 1) {
+                    // comparison within arr2:
+                    if (arr2[i - n] > arr2[i - n + gap]) swap(arr2, i - n, i - n + gap);
+                } else {
+                    // comparison within both arrays:
+                    if (arr1[i] > arr2[i + gap - n]) swap(arr1, arr2, i, i + gap - n);
+                }
+            }
+            gap = gap(gap);
+        }
+    }
+
+    private int gap(int gap) {
+        return gap == 1 ? 0 : gap / 2 + gap % 2;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int acc = arr[i];
+        arr[i] = arr[j];
+        arr[j] = acc;
+    }
+
+    private void swap(int[] arr1, int[] arr2, int i, int j) {
+        int acc = arr1[i];
+        arr1[i] = arr2[j];
+        arr2[j] = acc;
+    }
+
+    public static void main(String[] args) {
+        MergeWithoutExtraSpace mergeWithoutExtraSpace = new MergeWithoutExtraSpace();
+
+        int n = 4;
+        int m = 5;
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {0, 2, 6, 8, 9};
+
+        mergeWithoutExtraSpace.merge(arr1, arr2, n, m);
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+    }
 }
--- src/test/java/arrays/MergeWithoutExtraSpaceTest.java
+++ src/test/java/arrays/MergeWithoutExtraSpaceTest.java
@@ -1,2 +1,60 @@
-package arrays;public class MergeWithoutExtraSpaceTest {
+package arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MergeWithoutExtraSpaceTest {
+    public MergeWithoutExtraSpace mergeWithoutExtraSpace;
+
+    @Before
+    public void before() {
+        mergeWithoutExtraSpace = new MergeWithoutExtraSpace();
+    }
+
+    @Test
+    public void firstTestCase() {
+        int n = 4;
+        int m = 5;
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {0, 2, 6, 8, 9};
+
+        mergeWithoutExtraSpace.merge(arr1, arr2, n, m);
+
+        assertEquals(0, arr1[0]);
+        assertEquals(5, arr2[0]);
+    }
+
+    @Test
+    public void secondTestCase() {
+        int n = 2;
+        int m = 3;
+        int[] arr1 = {10, 12};
+        int[] arr2 = {5, 18, 20};
+
+        mergeWithoutExtraSpace.merge(arr1, arr2, n, m);
+
+        assertEquals(5, arr1[0]);
+        assertEquals(12, arr2[0]);
+    }
+
+    @Test
+    public void stackOverflowTest() {
+        int n = 50_000;
+        int m = 50_000;
+        int[] arr1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = 2 * i;
+        }
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = i % 2 == 0 ? i + 50_001 : i;
+        }
+
+        mergeWithoutExtraSpace.merge(arr1, arr2, n, m);
+
+        assertEquals(0, arr1[0]);
+        assertEquals(62512, arr2[0]);
+    }
 }
