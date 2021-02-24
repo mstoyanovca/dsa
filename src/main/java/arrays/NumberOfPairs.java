5bf9c615da7d0c7e720f2c10be6624cb1427b656
mstoyanovca <mstoyanovca@gmail.com>
2021-02-23 22:45:53 -0500

    "number of pairs"

---
+++ src/main/java/arrays/NumberOfPairs.java
@@ -0,0 +1,69 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class NumberOfPairs {
+    private final int[] exceptions = new int[4];
+
+    public long countPairs(int[] x, int[] y, int m, int n) {
+        if (m < 1 || m > 100_000) return 0;
+        if (n < 1 || n > 100_000) return 0;
+        for (int e : x) if (e < 1 || e > 1_000) return 0;
+        for (int e : y) if (e < 1 || e > 1_000) return 0;
+
+        for (int i = 0; i < n; i++) {
+            if (y[i] > 0 && y[i] < 5) exceptions[y[i] - 1]++;
+        }
+
+        int counter = 0;
+        Arrays.sort(y);
+
+        /*for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (Math.pow(x[i], y[j]) > Math.pow(y[j], x[i])) counter++;
+            }
+        }*/
+
+        for (int i = 0; i < m; i++) {
+            counter += count(x[i], y, n);
+        }
+
+        return counter;
+    }
+
+    private int count(int x, int[] y, int n) {
+        if (x == 1) return 0;
+        int index = nextGreaterElementIndex(x, y, n);
+        int count = index >= 0 ? n - index : 0;
+
+        count += exceptions[0];  // ones
+        if (x == 2) count -= exceptions[2] + exceptions[3];  // threes + fours
+        if (x == 3) count += exceptions[1];  // twos
+
+        return count;
+    }
+
+    private int nextGreaterElementIndex(int x, int[] y, int n) {
+        if (x >= y[n - 1]) return -1;
+        int index = Arrays.binarySearch(y, x);
+
+        if (index >= 0) {
+            while (index + 1 < n && y[index] <= y[index + 1]) index++;
+        } else {
+            index = -index - 1;
+        }
+
+        return index > n - 1 ? -1 : index;
+    }
+
+    public static void main(String[] args) {
+        NumberOfPairs numberOfPairs = new NumberOfPairs();
+
+        int m = 3;
+        int n = 2;
+        int[] x = {2, 1, 6};
+        int[] y = {1, 5};
+
+        System.out.println(numberOfPairs.countPairs(x, y, m, n));  // 3
+    }
+}
---
+++ src/test/java/arrays/NumberOfPairsTest.java
@@ -0,0 +1,49 @@
+package arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumberOfPairsTest {
+    private NumberOfPairs numberOfPairs;
+
+    @Before
+    public void before() {
+        numberOfPairs = new NumberOfPairs();
+    }
+
+    @Test
+    public void firstTestCase() {
+        int m = 3;
+        int n = 2;
+        int[] x = {2, 1, 6};
+        int[] y = {1, 5};
+
+        assertEquals(3, numberOfPairs.countPairs(x, y, m, n));
+    }
+
+    @Test
+    public void secondTestCase() {
+        int m = 4;
+        int n = 3;
+        int[] x = {2, 3, 4, 5};
+        int[] y = {1, 2, 3};
+
+        assertEquals(5, numberOfPairs.countPairs(x, y, m, n));
+    }
+
+    @Test
+    public void stackOverflowTest() {
+        int m = 100_000;
+        int n = 100_000;
+
+        int[] x = new int[m];
+        for (int i = 1; i <= m; i++) x[i - 1] = Math.min(i, 1_000);
+
+        int[] y = new int[n];
+        for (int i = 1; i <= n; i++) y[i - 1] = Math.min(i, 1_000);
+
+        assertEquals(100996, numberOfPairs.countPairs(x, y, m, n));
+    }
+}
