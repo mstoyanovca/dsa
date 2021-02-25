1d973e9bcad690e8305d05617be5053211da534f
mstoyanovca <mstoyanovca@gmail.com>
2021-02-24 20:58:22 -0500

    "number of pairs"

--- src/main/java/arrays/NumberOfPairs.java
+++ src/main/java/arrays/NumberOfPairs.java
@@ -3,14 +3,13 @@ package arrays;
 import java.util.Arrays;
 
 public class NumberOfPairs {
-    private final int[] exceptions = new int[4];
-
     public long countPairs(int[] x, int[] y, int m, int n) {
         if (m < 1 || m > 100_000) return 0;
         if (n < 1 || n > 100_000) return 0;
         for (int e : x) if (e < 1 || e > 1_000) return 0;
         for (int e : y) if (e < 1 || e > 1_000) return 0;
 
+        final int[] exceptions = new int[4];
         for (int i = 0; i < n; i++) {
             if (y[i] > 0 && y[i] < 5) exceptions[y[i] - 1]++;
         }
@@ -18,27 +17,21 @@ public class NumberOfPairs {
         int counter = 0;
         Arrays.sort(y);
 
-        /*for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (Math.pow(x[i], y[j]) > Math.pow(y[j], x[i])) counter++;
-            }
-        }*/
-
         for (int i = 0; i < m; i++) {
-            counter += count(x[i], y, n);
+            counter += count(x[i], y, n, exceptions);
         }
 
         return counter;
     }
 
-    private int count(int x, int[] y, int n) {
+    private int count(int x, int[] y, int n, int[] exceptions) {
         if (x == 1) return 0;
         int index = nextGreaterElementIndex(x, y, n);
         int count = index >= 0 ? n - index : 0;
 
-        count += exceptions[0];  // ones
-        if (x == 2) count -= exceptions[2] + exceptions[3];  // threes + fours
-        if (x == 3) count += exceptions[1];  // twos
+        count += exceptions[0];
+        if (x == 2) count -= exceptions[2] + exceptions[3];
+        if (x == 3) count += exceptions[1];
 
         return count;
     }
