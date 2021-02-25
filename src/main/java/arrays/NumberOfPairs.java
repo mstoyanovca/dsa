c17a835cd24bcce8d1dd1a92b4146c00ee260900
mstoyanovca <mstoyanovca@gmail.com>
2021-02-24 22:45:05 -0500

    "number of pairs"

--- src/main/java/arrays/NumberOfPairs.java
+++ src/main/java/arrays/NumberOfPairs.java
@@ -6,47 +6,42 @@ public class NumberOfPairs {
     public long countPairs(int[] x, int[] y, int m, int n) {
         if (m < 1 || m > 100_000) return 0;
         if (n < 1 || n > 100_000) return 0;
-        for (int e : x) if (e < 1 || e > 1_000) return 0;
-        for (int e : y) if (e < 1 || e > 1_000) return 0;
+        if (Arrays.stream(x).anyMatch(e -> e < 1 || e > 1_000)) return 0;
+        if (Arrays.stream(y).anyMatch(e -> e < 1 || e > 1_000)) return 0;
 
-        final int[] exceptions = new int[4];
-        for (int i = 0; i < n; i++) {
-            if (y[i] > 0 && y[i] < 5) exceptions[y[i] - 1]++;
-        }
+        final int[] exceptions = new int[5];
+        for (int i = 0; i < n; i++) if (y[i] < 5) exceptions[y[i]]++;
 
-        int counter = 0;
         Arrays.sort(y);
 
-        for (int i = 0; i < m; i++) {
-            counter += count(x[i], y, n, exceptions);
-        }
-
+        int counter = 0;
+        for (int i = 0; i < m; i++) counter += count(x[i], y, n, exceptions);
         return counter;
     }
 
     private int count(int x, int[] y, int n, int[] exceptions) {
-        if (x == 1) return 0;
-        int index = nextGreaterElementIndex(x, y, n);
-        int count = index >= 0 ? n - index : 0;
+        if (x == 0) return 0;
+        if (x == 1) return exceptions[0];
+        int count = n - ngeIndex(x, y, n);
 
-        count += exceptions[0];
-        if (x == 2) count -= exceptions[2] + exceptions[3];
-        if (x == 3) count += exceptions[1];
+        count += exceptions[0] + exceptions[1];
+        if (x == 2) count -= exceptions[3] + exceptions[4];
+        if (x == 3) count += exceptions[2];
 
         return count;
     }
 
-    private int nextGreaterElementIndex(int x, int[] y, int n) {
-        if (x >= y[n - 1]) return -1;
+    private int ngeIndex(int x, int[] y, int n) {
+        if (x >= y[n - 1]) return n;
         int index = Arrays.binarySearch(y, x);
 
         if (index >= 0) {
-            while (index + 1 < n && y[index] <= y[index + 1]) index++;
+            while (x == y[index]) index++;
         } else {
             index = -index - 1;
         }
 
-        return index > n - 1 ? -1 : index;
+        return index;
     }
 
     public static void main(String[] args) {
