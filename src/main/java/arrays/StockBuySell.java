ba762d89062475d95ddeadb5ebd324e61bc02b42
mstoyanovca <mstoyanovca@gmail.com>
2021-03-04 21:14:58 -0500

    "stock buy and sell"

--- src/main/java/arrays/StockBuySell.java
+++ src/main/java/arrays/StockBuySell.java
@@ -6,41 +6,46 @@ public class StockBuySell {
     private ArrayList<ArrayList<Integer>> stockBuySell(int[] a, int n) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
 
-        int minIndex = 0;
-        int maxIndex = 0;
-        for (int i = 1; i < n - 1; i++) {
-            if (a[i] < a[i - 1]) {
-                if (minIndex < maxIndex) {
-                    ArrayList<Integer> deals = new ArrayList<>();
-                    deals.add(minIndex);
-                    deals.add(maxIndex);
-                    result.add(deals);
-                }
-                minIndex = i;
-            }
-            if (a[i] < a[i + 1]) {
-                maxIndex = i + 1;
-            }
-        }
+        int minI = 0;
+        int maxI = 0;
 
-        if (minIndex < maxIndex) {
-            ArrayList<Integer> deals = new ArrayList<>();
-            deals.add(minIndex);
-            deals.add(maxIndex);
-            result.add(deals);
+        for (int i = 1; i < n; i++) {
+            if (a[i] < a[i - 1]) {
+                if (maxI > minI) addResult(result, minI, maxI);
+                minI = i;
+                maxI = i;
+            }
+            if (a[i] > a[i - 1]) maxI = i;
+            if (a[i] == a[i - 1]) {
+                if (maxI <= minI) minI = i;
+                maxI = i;
+            }
+            if (i == n - 1 && maxI > minI) addResult(result, minI, maxI);
         }
 
         return result;
     }
 
+    private void addResult(ArrayList<ArrayList<Integer>> result, int minI, int maxI) {
+        ArrayList<Integer> deal = new ArrayList<>();
+        deal.add(minI);
+        deal.add(maxI);
+        result.add(deal);
+    }
+
     public static void main(String[] args) {
         StockBuySell stockBuySell = new StockBuySell();
 
-        /*int n = 5;
-        int[] a = {74, 92, 10, 11, 3};*/
-        int n = 7;
-        int[] a = {100, 180, 260, 310, 40, 535, 695};
+        int n = 6;
+        int[] a1 = {82, 15, 91, 91, 58, 27};
+        System.out.println(stockBuySell.stockBuySell(a1, n));  // [1, 3]
 
-        System.out.println(stockBuySell.stockBuySell(a, n));  // [[0, 3], [4, 6]]  (0 1), (2 3)
+        n = 18;
+        int[] a2 = {11, 42, 49, 96, 23, 20, 49, 26, 26, 18, 73, 2, 53, 59, 34, 99, 25, 2};
+        System.out.println(stockBuySell.stockBuySell(a2, n));  // (0 3) (5 6) (9 10) (11 13) (14 15)
+
+        n = 7;
+        int[] a3 = {100, 180, 260, 310, 40, 535, 695};
+        System.out.println(stockBuySell.stockBuySell(a3, n));  // [[0, 3], [4, 6]]
     }
 }
