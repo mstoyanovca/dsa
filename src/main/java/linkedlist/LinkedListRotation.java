12ef6c4227b0187c9e2a580dd7fef976189c9892
mstoyanovca <mstoyanovca@gmail.com>
2021-03-18 20:16:44 -0400

    "rotate linked list"

---
+++ src/main/java/linkedlist/LinkedListRotation.java
@@ -0,0 +1,58 @@
+package linkedlist;
+
+public class LinkedListRotation {
+    private Node head;
+
+    public Node rotate(Node head, int k) {
+        Node k_p = head;
+        Node k1_p = head.next != null ? head.next : null;
+        Node tail = head;
+
+        int i = 1;
+        while (tail.next != null) {
+            tail = tail.next;
+            if (i < k) {
+                i++;
+                k_p = k_p.next;
+                k1_p = k_p.next;
+            }
+        }
+
+        if (k1_p != null) {
+            k_p.next = null;
+            tail.next = head;
+            head = k1_p;
+        }
+
+        return head;
+    }
+
+    public void add(int data) {
+        Node node = new Node(data);
+        node.next = head;
+        head = node;
+    }
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListRotation linkedList = new LinkedListRotation();
+        for (int i = 8; i > 0; i--) linkedList.add(i);
+
+        System.out.println(linkedList.rotate(linkedList.head, 4));  // 5
+    }
+}
