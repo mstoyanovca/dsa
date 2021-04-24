03df1e8971d6a458535e39ee185b077e722784f9
mstoyanovca <mstoyanovca@gmail.com>
2021-04-23 23:52:33 -0400

    "flood fill DFS"

--- src/main/java/recursion/FloodFill.java
+++ src/main/java/recursion/FloodFill.java
@@ -2,72 +2,84 @@ package recursion;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 public class FloodFill {
     public int[][] floodFill(int[][] image, int row, int column, int newColor) {
         int previousColor = image[row][column];
-        int[][] visited = new int[image[0].length][image.length];
-        Queue<List<Integer>> queue = new LinkedList<>();
+        int[][] visited = new int[image.length][image[0].length];
+        Queue<Pixel> queue = new LinkedList<>();
 
-        queue.add(Arrays.asList(row, column));
+        queue.add(new Pixel(row, column));
         visited[row][column] = 1;
 
         while (!queue.isEmpty()) {
-            List<Integer> tuple = queue.remove();
-            int x = tuple.get(0);
-            int y = tuple.get(1);
+            Pixel pixel = queue.remove();
+            int r = pixel.row;
+            int c = pixel.column;
 
-            image[x][y] = newColor;
+            image[r][c] = newColor;
 
-            if (x - 1 >= 0 && image[x - 1][y] == previousColor && visited[x - 1][y] == 0) {
-                visited[x - 1][y] = 1;
-                queue.add(Arrays.asList(x - 1, y));
+            if (r - 1 >= 0 && image[r - 1][c] == previousColor && visited[r - 1][c] == 0) {
+                visited[r - 1][c] = 1;
+                queue.add(new Pixel(r - 1, c));
             }
-            if (x + 1 < image[0].length && image[x + 1][y] == previousColor && visited[x + 1][y] == 0) {
-                visited[x + 1][y] = 1;
-                queue.add(Arrays.asList(x + 1, y));
+            if (r + 1 < image.length && image[r + 1][c] == previousColor && visited[r + 1][c] == 0) {
+                visited[r + 1][c] = 1;
+                queue.add(new Pixel(r + 1, c));
             }
-            if (y - 1 >= 0 && image[x][y - 1] == previousColor && visited[x][y - 1] == 0) {
-                visited[x][y - 1] = 1;
-                queue.add(Arrays.asList(x, y - 1));
+            if (c - 1 >= 0 && image[r][c - 1] == previousColor && visited[r][c - 1] == 0) {
+                visited[r][c - 1] = 1;
+                queue.add(new Pixel(r, c - 1));
             }
-            if (y + 1 < image.length && image[x][y + 1] == previousColor && visited[x][y + 1] == 0) {
-                visited[x][y + 1] = 1;
-                queue.add(Arrays.asList(x, y + 1));
+            if (c + 1 < image[0].length && image[r][c + 1] == previousColor && visited[r][c + 1] == 0) {
+                visited[r][c + 1] = 1;
+                queue.add(new Pixel(r, c + 1));
             }
         }
 
         return image;
     }
 
+    private static class Pixel implements Comparable<Pixel> {
+        int row;
+        int column;
+
+        public Pixel(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        @Override
+        public int compareTo(Pixel pixel) {
+            return row - pixel.row + column - pixel.column;
+        }
+    }
+
     // recursive:
-    private void loop(int[][] image, int row, int column, int oldColor, int newColor) {
+    private void loop(int[][] image, int row, int column, int previousColor, int newColor) {
         if (row < 0 || row > image.length - 1) return;
         if (column < 0 || column > image[0].length - 1) return;
-        if (image[row][column] != oldColor) return;
+        if (image[row][column] != previousColor) return;
 
         image[row][column] = newColor;
 
         // left:
-        loop(image, row, column - 1, oldColor, newColor);
+        loop(image, row, column - 1, previousColor, newColor);
         // right:
-        loop(image, row, column + 1, oldColor, newColor);
+        loop(image, row, column + 1, previousColor, newColor);
         // top:
-        loop(image, row - 1, column, oldColor, newColor);
+        loop(image, row - 1, column, previousColor, newColor);
         // bottom:
-        loop(image, row + 1, column, oldColor, newColor);
+        loop(image, row + 1, column, previousColor, newColor);
     }
 
     public static void main(String[] args) {
-        int[][] arr = {{1, 2}, {3, 4}, {5, 6}};
-
         int[][] image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
         int row = 1;
         int column = 1;
         int newColor = 2;
 
-        System.out.println(Arrays.deepToString(new FloodFill().floodFill(image, row, column, newColor)));
+        System.out.println(Arrays.deepToString(new FloodFill().floodFill(image, row, column, newColor)));  // {{2,2,2},{2,2,0},{2,0,1}}
     }
 }
