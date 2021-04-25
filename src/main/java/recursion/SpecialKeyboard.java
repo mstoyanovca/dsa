e9df8ec616897329e486bd1cda57c347d4ee0696
mstoyanovca <mstoyanovca@gmail.com>
2021-04-24 23:03:08 -0400

    "special keyboard"

--- src/main/java/recursion/SpecialKeyboard.java
+++ src/main/java/recursion/SpecialKeyboard.java
@@ -4,6 +4,19 @@ public class SpecialKeyboard {
     public int optimalKeys(int n) {
         if (n < 7) return n;
 
+        int[] results = new int[n];
+        for (int i = 0; i < 6; i++) results[i] = i + 1;
+
+        for (int i = 7; i <= n; i++)
+            for (int bp = i - 3; bp >= 1; bp--)
+                results[i - 1] = Math.max(results[i - 1], (i - bp - 1) * results[bp - 1]);
+
+        return results[n - 1];
+    }
+
+    public int optimalKeysRecursive(int n) {
+        if (n < 7) return n;
+
         int max = 0;
         for (int bp = n - 3; bp > 0; bp--) max = Math.max(max, (n - bp - 1) * optimalKeys(bp));
 
