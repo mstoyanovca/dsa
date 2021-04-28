812c8a94e261d693a1990b8d84303a85d4e10d12
mstoyanovca <mstoyanovca@gmail.com>
2021-04-27 20:27:42 -0400

    "largest sub-array with sum 0"

--- src/main/java/hashing/LargestSubArrayWithSum0.java
+++ src/main/java/hashing/LargestSubArrayWithSum0.java
@@ -11,6 +11,7 @@ public class LargestSubArrayWithSum0 {
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += arr[i];
+            if (sum == 0) maxLength = i + 1;
             if (!hash.containsKey(sum)) {
                 hash.put(sum, i);
             } else {
@@ -26,5 +27,9 @@ public class LargestSubArrayWithSum0 {
         int[] arr = {-341, 778, -826, -153, -574, -289, -993, -622, -989, -695, 150, -692, 755, -150, -586, -123, 960, -182, -605, 168, -635, 47, -108, 126, 158, 71, -584, -482, 565, -51, 369, -431, 431, 467, 305, 572, -793, 276, 639, -706, 574, 158, 894, -849, 979, -959, 432, -25, 712, -897, -476, 661, 791, 880, -686, -278, 364, -123, 429, -65, 230, 459, -770, -872, 330, -202, -944, 783, -502, 869, -246, -154, -935, 572, 959, -475, 18, -198, -769};
 
         System.out.println(new LargestSubArrayWithSum0().maxLen(arr, n));  // 10
+
+        n = 4;
+        int[] arr2 = {-1, 1, -1, 1};
+        System.out.println(new LargestSubArrayWithSum0().maxLen(arr2, n));  // 4
     }
 }
