08677f4c3b7bf4acfcb94e4b3a5285bb8be4ea49
mstoyanovca <mstoyanovca@gmail.com>
2021-05-01 20:46:58 -0400

    "all pairs with given sum"

--- src/main/java/hashing/AllPairsWithGivenSum.java
+++ src/main/java/hashing/AllPairsWithGivenSum.java
@@ -5,26 +5,12 @@ import java.util.*;
 public class AllPairsWithGivenSum {
     public Pair[] allPairs(long[] a, long[] b, long m, long n, long sum) {
         Map<Long, Integer> hash = new HashMap<>();
-        for (long l : a) {
-            if (!hash.containsKey(l)) {
-                hash.put(l, 1);
-            } else {
-                hash.put(l, hash.get(l) + 1);
-            }
-        }
+        for (long l : a) hash.put(l, 1);
 
         List<Pair> pairs = new ArrayList<>();
-        for (long l : b) {
-            if (hash.containsKey(sum - l)) {
-                pairs.add(new Pair(sum - l, l));
-                if (hash.get(sum - l) == 1) {
-                    hash.remove(sum - l);
-                } else {
-                    hash.put(sum - l, hash.get(sum - l) - 1);
-                }
-            }
-        }
+        for (long l : b) if (hash.containsKey(sum - l)) pairs.add(new Pair(sum - l, l));
 
+        pairs.sort((Pair p1, Pair p2) -> (int) (p1.first - p2.first));
         return pairs.toArray(new Pair[0]);
     }
 
@@ -46,7 +32,7 @@ public class AllPairsWithGivenSum {
         long m = 5;
         long n = 5;
         long sum = 9;
-        long[] a = {1, 2, 4, 5, 7};
+        long[] a = {-1, 2, 4, 5, 7};
         long[] b = {5, 6, 3, 4, 8};
 
         System.out.println(Arrays.toString(new AllPairsWithGivenSum().allPairs(a, b, m, n, sum)));  // (1, 8), (4, 5), (5, 4)
