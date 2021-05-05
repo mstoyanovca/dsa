caed19021a0c1b9c25e25b6e191e5cb8e9a751ac
mstoyanovca <mstoyanovca@gmail.com>
2021-05-04 23:16:53 -0400

    "smallest window"

--- src/main/java/hashing/SmallestWindow.java
+++ src/main/java/hashing/SmallestWindow.java
@@ -6,46 +6,35 @@ import java.util.Map;
 public class SmallestWindow {
     public String smallestWindow(String s, String p) {
         if (s.length() < p.length()) return "-1";
-        if (s.length() == 1) return s.equalsIgnoreCase(p) ? p : "-1";
 
-        Map<Character, Integer> hash = new HashMap<>();
-        for (int i = 0; i < p.length(); i++)
-            hash.put(p.charAt(i), hash.get(p.charAt(i)) == null ? 1 : hash.get(p.charAt(i)) + 1);
-        int left = -1;
-        int right = -1;
+        Map<Character, Integer> hash_p = new HashMap<>();
+        Map<Character, Integer> hash_s = new HashMap<>();
+        int count = 0;
+        int left = 0;
         int start = -1;
         int min = s.length() + 1;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (hash.get(s.charAt(i)) != null && hash.get(s.charAt(i)) > 0) {
-                left = i;
-                hash.put(s.charAt(i), hash.get(s.charAt(i)) - 1);
-                if (left + 1 >= s.length()) return "-1";
-                break;
-            }
-        }
-        if (left == -1) return "-1";
+        for (int i = 0; i < p.length(); i++)
+            hash_p.put(p.charAt(i), hash_p.get(p.charAt(i)) == null ? 1 : hash_p.get(p.charAt(i)) + 1);
 
-        for (int i = left + 1; i < s.length(); i++) {
-            if (hash.get(s.charAt(i)) != null && hash.get(s.charAt(i)) > 0) {
-                hash.put(s.charAt(i), hash.get(s.charAt(i)) - 1);
-                if (hash.values().stream().noneMatch(e -> e != 0)) {
+        for (int i = 0; i < s.length(); i++) {
+            if (hash_p.containsKey(s.charAt(i))) {
+                hash_s.put(s.charAt(i), hash_s.get(s.charAt(i)) == null ? 1 : hash_s.get(s.charAt(i)) + 1);
+                if (hash_s.get(s.charAt(i)) <= hash_p.get(s.charAt(i))) count++;
+
+                if (count == p.length()) {
+                    while (!hash_p.containsKey(s.charAt(left)) || hash_s.containsKey(s.charAt(left)) && hash_s.get(s.charAt(left)) > hash_p.get(s.charAt(left))) {
+                        if (hash_s.containsKey(s.charAt(left)) && hash_s.get(s.charAt(left)) > hash_p.get(s.charAt(left)))
+                            hash_s.put(s.charAt(left), hash_s.get(s.charAt(left)) - 1);
+                        left++;
+                    }
+                    min = Math.min(min, i - left + 1);
                     start = left;
-                    right = i;
-                    min = Math.min(min, right - left + 1);
-                    break;
                 }
             }
         }
-        if (start == -1) return "-1";
 
-        while (right < s.length()) {
-            char removed = s.charAt(left++);
-            while (!hash.containsKey(s.charAt(left))) left++;
-            while (s.charAt(right) == removed) right++;
-        }
-
-        return start == -1 ? "-1" : s.substring(start, start + min);
+        return start == -1 ? "-1" : s.substring(left, left + min);
     }
 
     public static void main(String[] args) {
