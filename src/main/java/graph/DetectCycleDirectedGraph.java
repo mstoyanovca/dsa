e291eb6a2c05a7f2731cc6ceda75a35923cd6b1e
mstoyanovca <mstoyanovca@gmail.com>
2021-05-10 22:48:01 -0400

    detect cycle in a directed path

--- src/main/java/graph/DetectCycleDirectedGraph.java
+++ src/main/java/graph/DetectCycleDirectedGraph.java
@@ -6,36 +6,43 @@ import java.util.Collections;
 public class DetectCycleDirectedGraph {
     public boolean isCyclic(int v, ArrayList<ArrayList<Integer>> graph) {
         boolean[] visited = new boolean[v];
-        boolean[] stack = new boolean[v];
-        for (int i = 0; i < v; i++) {
-            if (!visited[i]) if (loop(0, visited, stack, graph)) {
+        boolean[] beingVisited = new boolean[v];
+
+        for (int i = 0; i < v; i++)
+            if (!visited[i] && loop(i, visited, beingVisited, graph))
                 return true;
-            }
-        }
+
         return false;
     }
 
-    private boolean loop(int node, boolean[] visited, boolean[] stack, ArrayList<ArrayList<Integer>> graph) {
-        if (stack[node]) return true;
-        if (visited[node]) return false;
-        visited[node] = true;
-        stack[node] = true;
+    private boolean loop(int node, boolean[] visited, boolean[] beingVisited, ArrayList<ArrayList<Integer>> graph) {
+        beingVisited[node] = true;
 
         for (int i : graph.get(node)) {
-            if (loop(i, visited, stack, graph)) return true;
+            if (beingVisited[i]) {
+                return true;
+            } else if (!visited[i] && loop(i, visited, beingVisited, graph)) {
+                return true;
+            }
         }
-        stack[node] = false;
+
+        beingVisited[node] = false;
+        visited[node] = true;
+
         return false;
     }
 
     public static void main(String[] args) {
-        int v = 4;
+        int v = 6;
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        graph.add(new ArrayList<>(Collections.singletonList(1)));  // 0
+        graph.add(new ArrayList<>(Collections.emptyList()));       // 0
         graph.add(new ArrayList<>(Collections.singletonList(2)));  // 1
-        graph.add(new ArrayList<>(Collections.singletonList(3)));  // 2
-        graph.add(new ArrayList<>(Collections.singletonList(3)));  // 3
+        graph.add(new ArrayList<>(Collections.singletonList(4)));  // 2
+        graph.add(new ArrayList<>(Collections.singletonList(1)));  // 3
+        graph.add(new ArrayList<>(Collections.singletonList(0)));  // 4
+        graph.add(new ArrayList<>(Collections.singletonList(3)));  // 5
+        graph.add(new ArrayList<>(Collections.singletonList(5)));  // 6
 
-        System.out.println(new DetectCycleDirectedGraph().isCyclic(v, graph));  // true
+        System.out.println(new DetectCycleDirectedGraph().isCyclic(v, graph));  // false
     }
 }
