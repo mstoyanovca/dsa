63f508b26cd9f9c5bed139c9a7d35e96d05e22ba
mstoyanovca <mstoyanovca@gmail.com>
2021-05-17 19:37:52 -0400

    dijkstra algorithm

--- src/main/java/graph/MinimumSwapsToSort.java
+++ src/main/java/graph/MinimumSwapsToSort.java
@@ -1,26 +1,25 @@
 package graph;
 
-import javafx.util.Pair;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class MinimumSwapsToSort {
     public int minSwaps(int[] nums) {
         int result = 0;
         boolean[] visited = new boolean[nums.length];
-        ArrayList<Pair<Integer, Integer>> numberIndex = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) numberIndex.add(new Pair<>(nums[i], i));
-        numberIndex.sort(Comparator.comparingInt(Pair::getKey));
+        ArrayList<ArrayList<Integer>> numberIndex = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) numberIndex.add(new ArrayList<>(Arrays.asList(nums[i], i)));
+        numberIndex.sort(Comparator.comparingInt(o -> o.get(0)));
 
         for (int i = 0; i < nums.length; i++) {
-            if (visited[i] || numberIndex.get(i).getValue() == i) continue;
+            if (visited[i] || numberIndex.get(i).get(1) == i) continue;
 
             int swaps = 0;
             int j = i;
             while (!visited[j]) {
                 visited[j] = true;
-                j = numberIndex.get(j).getValue();
+                j = numberIndex.get(j).get(1);
                 swaps++;
             }
             if (swaps > 0) result += swaps - 1;
