43cec9962e7cbf5bc22879ab1481b3561073ee99
mstoyanovca <mstoyanovca@gmail.com>
2021-06-01 20:01:23 -0400

    minimum cost queue

--- src/main/java/graph/MinimumCostGraph.java
+++ src/main/java/graph/MinimumCostGraph.java
@@ -1,26 +1,26 @@
 package graph;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
 public class MinimumCostGraph {
     public int minimumCostPath(int[][] grid) {
         int n = grid.length;
-        List<List<Integer>> graph = new ArrayList<>(n * n);  // indexes
+        boolean[] visited = new boolean[n * n];
+        int[] costs = new int[n * n];
+        int[] distances = new int[n * n];
+        for (int i = 0; i < n * n; i++) distances[i] = Integer.MAX_VALUE;
+        distances[0] = grid[0][0];
+        List<List<Integer>> graph = new ArrayList<>(n * n);
         for (int i = 0; i < n * n; i++) graph.add(new ArrayList<>());
-        List<Integer> costs = new ArrayList<>(n * n);
-        List<Integer> distances = new ArrayList<>(n * n);
-        for (int i = 0; i < n * n; i++) distances.add(Integer.MAX_VALUE);
-        distances.set(0, 0);
-        Queue<Integer> queue = new LinkedList<>();  // indexes
+        PriorityQueue<Integer> queue = new PriorityQueue<>(n * n);
         queue.add(0);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                costs.add(grid[i][j]);
                 int vertex = i * n + j;
+                costs[vertex] = grid[i][j];
 
                 if (i - 1 >= 0) graph.get(vertex).add(vertex - n);
                 if (i + 1 < n) graph.get(vertex).add(vertex + n);
@@ -31,17 +31,22 @@ public class MinimumCostGraph {
 
         while (!queue.isEmpty()) {
             int vertex = queue.poll();
-            List<Integer> adj = graph.get(vertex);
 
-            for (int i : adj) {
-                if (distances.get(i) > costs.get(vertex) + costs.get(i)) {
-                    distances.set(i, costs.get(vertex) + costs.get(i));
-                    queue.add(i);
+            if (!visited[vertex]) {
+                visited[vertex] = true;
+
+                for (int i : graph.get(vertex)) {
+                    if (!visited[i]) {
+                        if (distances[i] > distances[vertex] + costs[i]) {
+                            distances[i] = distances[vertex] + costs[i];
+                            queue.add(i);
+                        }
+                    }
                 }
             }
         }
 
-        return distances.get(n * n - 1);
+        return distances[n * n - 1];
     }
 
     public static void main(String[] args) {
