9d7deab2b8a4948adf57fa8c000e43b1c98c82ff
mstoyanovca <mstoyanovca@gmail.com>
2021-06-03 21:44:59 -0400

    circle of strings

--- src/main/java/graph/CircleOfStrings.java
+++ src/main/java/graph/CircleOfStrings.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 public class CircleOfStrings {
     private static final int CHARS = 26;
-    private final int[] inDegree = new int[CHARS];
+    private final List<List<Integer>> graph;
+    private final int[] inDegree;
+
+    public CircleOfStrings() {
+        graph = new ArrayList<>(CHARS);
+        for (int i = 0; i < CHARS; i++) graph.add(new ArrayList<>());
+        inDegree = new int[CHARS];
+    }
 
     public int isCircle(int vertices, String[] arr) {
-        List<List<Integer>> graph = new ArrayList<>(CHARS);
-        for (int i = 0; i < CHARS; i++) graph.add(new ArrayList<>());
-
         for (int i = 0; i < vertices; i++) {
             String s = arr[i];
             int first = s.charAt(0) - 'a';
@@ -19,44 +23,45 @@ public class CircleOfStrings {
             inDegree[last]++;
         }
 
-        return hasEulerianCircuit(vertices, graph) ? 1 : 0;
+        return hasEulerianCircuit() ? 1 : 0;
     }
 
-    private boolean hasEulerianCircuit(int vertices, List<List<Integer>> graph) {
-        if (!isStronglyConnected(vertices, graph)) return false;
-        for (int i = 0; i < vertices; i++) if (graph.get(i).size() != inDegree[i]) return false;
+    private boolean hasEulerianCircuit() {
+        if (!isStronglyConnected()) return false;
+        for (int i = 0; i < CHARS; i++) if (graph.get(i).size() != inDegree[i]) return false;
         return true;
     }
 
-    private boolean isStronglyConnected(int vertices, List<List<Integer>> graph) {
-        boolean[] visited = new boolean[vertices];
-        dfs(0, visited, graph);
-        for (int i = 0; i < vertices; i++) if (!visited[i]) return false;
+    private boolean isStronglyConnected() {
+        // the first vertex with non-zero degree:
+        int start = -1;
+        for (int i = 0; i < CHARS; i++) {
+            if (graph.get(i).size() > 0) {
+                start = i;
+                break;
+            }
+        }
+        boolean[] visited = new boolean[CHARS];
 
-        for (int i = 0; i < vertices; i++) visited[i] = false;
-        List<List<Integer>> reverseGraph = transpose(vertices, graph);
-        dfs(0, visited, reverseGraph);
-        for (int i = 0; i < vertices; i++) if (!visited[i]) return false;
+        dfs(start, visited, graph);
+        for (int i = 0; i < CHARS; i++) if (graph.get(i).size() > 0 && !visited[i]) return false;
+
+        for (int i = 0; i < CHARS; i++) visited[i] = false;
+        dfs(start, visited, transpose());
+        for (int i = 0; i < CHARS; i++) if (graph.get(i).size() > 0 && !visited[i]) return false;
 
         return true;
     }
 
     private void dfs(int start, boolean[] visited, List<List<Integer>> graph) {
         visited[start] = true;
-        for (int i : graph.get(start))
-            if (!visited[i])
-                dfs(i, visited, graph);
+        for (int i : graph.get(start)) if (!visited[i]) dfs(i, visited, graph);
     }
 
-    private List<List<Integer>> transpose(int vertices, List<List<Integer>> graph) {
+    private List<List<Integer>> transpose() {
         List<List<Integer>> reverseGraph = new ArrayList<>(CHARS);
         for (int i = 0; i < CHARS; i++) reverseGraph.add(new ArrayList<>());
-        for (int i = 0; i < vertices; i++) {
-            for (int j : graph.get(i)) {
-                reverseGraph.get(j).add(i);
-                inDegree[i]++;
-            }
-        }
+        for (int i = 0; i < CHARS; i++) for (int j : graph.get(i)) reverseGraph.get(j).add(i);
         return reverseGraph;
     }
 
@@ -64,7 +69,7 @@ public class CircleOfStrings {
         String[] a = {"abc", "bcd", "cdf"};
         String[] b = {"ab", "bc", "cd", "da"};
 
-        System.out.println(new CircleOfStrings().isCircle(3, a));  // false
-        System.out.println(new CircleOfStrings().isCircle(4, b));  // true
+        System.out.println(new CircleOfStrings().isCircle(3, a));  // 0
+        System.out.println(new CircleOfStrings().isCircle(4, b));  // 1
     }
 }
