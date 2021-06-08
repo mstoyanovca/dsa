b8c4acb38949f40b7833e05e179c54dba03e756e
mstoyanovca <mstoyanovca@gmail.com>
2021-06-07 20:25:37 -0400

    Floyd Warshall

--- src/main/java/graph/FloydWarshall.java
+++ src/main/java/graph/FloydWarshall.java
@@ -5,24 +5,22 @@ import java.util.Arrays;
 public class FloydWarshall {
     public void shortest_distance(int[][] matrix) {
         int vertices = matrix.length;
-        int[][] distances = new int[vertices][vertices];
-        for (int i = 0; i < vertices; i++) {
-            for (int j = 0; j < vertices; j++) {
-                distances[i][j] = matrix[i][j];
-            }
-        }
 
         for (int v = 0; v < vertices; v++) {
             for (int i = 0; i < vertices; i++) {
                 for (int j = 0; j < vertices; j++) {
-                    if (distances[i][v] + distances[v][j] < distances[i][j]) {
-                        distances[i][j] = distances[i][v] + distances[v][j];
-                    }
+                    if (matrix[i][j] == -1) matrix[i][j] = 1_000;
+                    if (matrix[i][v] == -1) matrix[i][v] = 1_000;
+                    if (matrix[v][j] == -1) matrix[v][j] = 1_000;
+
+                    if (matrix[i][j] > matrix[i][v] + matrix[v][j]) matrix[i][j] = matrix[i][v] + matrix[v][j];
+
+                    if (matrix[i][j] == 1_000) matrix[i][j] = -1;
+                    if (matrix[i][v] == 1_000) matrix[i][v] = -1;
+                    if (matrix[v][j] == 1_000) matrix[v][j] = -1;
                 }
             }
         }
-
-        int x = 0;
     }
 
     public static void main(String[] args) {
@@ -37,7 +35,7 @@ public class FloydWarshall {
         new FloydWarshall().shortest_distance(matrix1);
         new FloydWarshall().shortest_distance(matrix2);
 
-        System.out.println(Arrays.toString(matrix1));  // {{0, 25}, {-1, 0}};
-        System.out.println(Arrays.toString(matrix1));  // {{0,1,7},{1,0,6},{-1,-1,0}}
+        System.out.println(Arrays.deepToString(matrix1));  // {{0, 25}, {-1, 0}};
+        System.out.println(Arrays.deepToString(matrix2));  // {{0,1,7},{1,0,6},{-1,-1,0}}
     }
 }
